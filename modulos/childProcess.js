babff61f5f8db9ef1531f504d31b21fdb28a9ba2
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-09-28 01:38:49 -0500

    exec vs spawn.

--- modulos/childProcess.js
+++ modulos/childProcess.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 
 exec('ls -la', (err, stdout, sterr) => {
     if (err) {
@@ -9,4 +9,31 @@ exec('ls -la', (err, stdout, sterr) => {
     console.log(stdout);
 });
 
+
+exec('node console.js', (err, stdout, sterr) => {
+    if (err) {
+        console.log(err);
+        return false;
+    }
+
+    console.log(stdout);
+});
+
+
+// Spawn
+let process = spawn('ls', ['-la']);
+
+console.log(process.pid);
+console.log(process.connected);
+
+// NodeJS es orientado a eventos, por lo tanto:
+process.stdout.on('data', function(info){
+    console.log(process.killed);
+    console.log(info.toString());
+});
+
+process.on('exit', function(){
+    console.log('Process ended.');
+    console.log(process.killed);
+})
 
