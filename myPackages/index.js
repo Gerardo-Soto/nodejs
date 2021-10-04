0e8033c0fd7932a0d5c29d733bc70a868adf19a3
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-10-04 15:55:55 -0500

    myPackages/index.js ready

--- myPackages/index.js
+++ myPackages/index.js
@@ -2,7 +2,8 @@
 const myModule = require('./modulo.js');
 
 // execute module
+console.log(myModule);
 console.log(myModule.greeting);
-myModule.farewell();
-
+//myModule.farewell();  // <- farewall is not a function
+console.log(myModule.farewell);
 
