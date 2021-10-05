d1903319b6d8333d62ff1c30d5885d60a20aa990
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-10-04 21:48:37 -0500

    moment test.

---
+++ utiles/moment_test.js
@@ -0,0 +1,16 @@
+//moment
+const moment = require('moment');
+
+let now = moment();
+
+console.log(now);
+console.log(now.format('YYYY-MM-DD'));
+console.log(now.format('YYYY/MM/DD - HH:mm'));
+
+let startWort = moment().subtract('2', 'M');
+console.log('Start to work:');
+console.log(startWort);
+
+let daysWork = now.subtract(startWort);
+console.log('Days at work: '+ daysWork);
+
