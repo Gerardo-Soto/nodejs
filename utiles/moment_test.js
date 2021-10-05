8a04f6c54645bf67c9e87b420dab98d0e6aae701
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-10-04 22:15:00 -0500

    fix calc.

--- utiles/moment_test.js
+++ utiles/moment_test.js
@@ -11,6 +11,6 @@ let startWort = moment().subtract('2', 'M');
 console.log('Start to work:');
 console.log(startWort);
 
-let daysWork = now.subtract(startWort).toDate();
+let daysWork = startWort.subtract(now).toDate();
 console.log('Days at work: '+ daysWork);
 
