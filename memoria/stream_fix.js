b95964d9bc2e1180e1f7b57712a6525520c8eaa6
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-10-06 14:15:05 -0500

    test settings to Stream.

--- memoria/stream_fix.js
+++ memoria/stream_fix.js
@@ -6,8 +6,18 @@ let data = '';
 
 let readableStream = fs.createReadStream(__dirname + '/input.txt');
 
+
+/** Here we are not going to use a Callback because:
+ * the Callback is executed when something finishes executing,
+ * and with Streams we will be receiving data from time to time.
+ */
+readableStream.setEncoding('utf-8');
+// for each event: on.[data] run: 
 readableStream.on('data', function (chunk) {
-    console.log(chunk.toString());
-})
+    //console.log(chunk); // Test 1
+    data += chunk
+});
 
-
+readableStream.on('end', function () {
+    console.log(data);
+});
