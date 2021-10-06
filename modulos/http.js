5b190c89d23c311e68fc6621d97d925e867db512
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-10-06 15:09:44 -0500

    web server with debugger

---
+++ modulos/http.js
@@ -0,0 +1,23 @@
+// Web server
+const http = require('http');
+const port = 3000
+
+http.createServer(router).listen(port);
+
+function router(req, res) {
+    console.log('New request!');
+    console.log(req.url);
+
+    switch (req.url) {
+        case '/hello':
+            res.write('Hello, World! How are you?');
+            res.end();
+            break;
+    
+        default:
+            res.write('Error 404: Page not found');
+            res.end();
+    }
+}
+
+console.log('Listening http on port: '+ port);
