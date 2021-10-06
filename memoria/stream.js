099d55dad708955820b6617c0dcb45f7baf38a8c
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-10-06 13:24:31 -0500

    test memory buffers.

---
+++ memoria/stream.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const stream = require('stream');
+const util = require('util');
+
+let data = '';
+
+let readableStream = fs.createReadStream(__dirname + '/input.txt');
+
+// Transformación para el tratado del Stream:
+const Transform = stream.Transform;
+
+function UpperLetter(){
+    Transform.call(this); // *1
+}
+util.inherits(upperLetter, Transform);
+
+upperLetter.prototype._transform = function(chuck, codif, cb){// *1
+    chunkUpper = chuck.toString().toUpperCase();
+    this.push(chunkUpper);
+    cb();
+}
+
+// Ejecución:
+let upperLetter = new UpperLetter();
+
+readableStream
+    .pipe(UpperLetter)// send Stream
+    .pipe(process.stdout);// show the output on terminal
+
+
+
+/** Here we are not going to use a Callback because:
+ * the Callback is executed when something finishes executing,
+ * and with Streams we will be receiving data from time to time.
+ */
+readableStream.setEncoding('utf-8');
+// for each event: on.[data] run: 
+readableStream.on('data', function(chunk){
+    //console.log(chunk); // Test 1
+    data += chunk
+});
+
+readableStream.on('end', function(){
+    console.log(data);
+});
+
